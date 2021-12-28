b8cd6f7db5a13fee3933002f5223a7de363b855d
Augusto Sacuaia <augusto.sacuaia@pt.softinsa.com>
2021-12-28 17:47:10 +0000

    RestTemplate

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/payments")
 public class PaymentResource {
     
+ 
     @Autowired
     private PaymentService service;
 
