fa0d46d4d4dd491389e7f78659cd9b46104ff112
Augusto Sacuaia <augusto.sacuaia@pt.softinsa.com>
2022-01-03 15:15:34 +0000

    Config

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -8,8 +8,10 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertyResolver;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,23 +23,28 @@ public class WorkerResource {
     
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Autowired
-    private  Environment env ;
-    
+    @Value("${test.config}")
+	private String testConfig;
+	@Autowired
+	private WorkerRepository repository;
 
-    @Autowired
-    private WorkerRepository repository;
-    
-    @GetMapping
-    public ResponseEntity<List<Worker>> findAll(){
-        List<Worker> list = repository.findAll();
-        return ResponseEntity.ok(list);
-    }
+	private PropertyResolver env;
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll() {
+		List<Worker> list = repository.findAll();
+		return ResponseEntity.ok(list);
+	}
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG  " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 
     @GetMapping(value = "/{id}")
     public ResponseEntity <Worker> findById(@PathVariable Long id){
         
-        logger.info("PORT =" + env.getProperty("local.server.port"));
+        logger.info("PORT =" + env.getProperty("local.server.port") );
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
